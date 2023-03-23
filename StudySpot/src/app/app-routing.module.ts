4282960e8928e2c140f1b9a9bab583bfee2e908b
Singh <deepak.s.singh@capgemini.com>
2023-03-23 16:07:21 +0530

    updating the home file

--- StudySpot/src/app/app-routing.module.ts
+++ StudySpot/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeNavbarComponent } from './home/home-navbar/home-navbar.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'navbar', component: HomeNavbarComponent }
 ];
 
 @NgModule({
--- StudySpot/src/app/app.module.ts
+++ StudySpot/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HomeNavbarComponent } from './home/home-navbar/home-navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomeNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
