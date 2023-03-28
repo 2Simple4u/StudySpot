d84ba10b04d400661872b6421c844dca233b1f4b
Singh <deepak.s.singh@capgemini.com>
2023-03-28 14:55:21 +0530

    files od signup is attached with loginform

--- StudySpot/src/app/app-routing.module.ts
+++ StudySpot/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeNavbarComponent } from './home/home-navbar/home-navbar.component';
 import { HomeComponent } from './home/home.component';
-
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
+  
   { path: '', component: HomeComponent },
   { path: 'navbar', component: HomeNavbarComponent },
   { path: 'login', component:LoginPageComponent},
