997594259adbd3a3c502c9670a5aad8e42116523
Singh <deepak.s.singh@capgemini.com>
2023-03-28 18:20:38 +0530

    new services file is added

---
+++ StudySpot/src/app/services/customvalidation.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomvalidationService {
+
+  MatchPassword(password: string, confirmPassword: string) {
+    
+   
+      }
+
+  constructor() { }
+}
--- StudySpot/src/app/signup-page/signup-page.component.ts
+++ StudySpot/src/app/signup-page/signup-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormGroup, FormBuilder } from '@angular/forms'
+import { CustomvalidationService } from '../services/customvalidation.service';
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
@@ -7,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPageComponent implements OnInit {
 
-  constructor() { }
+  registerForm:FormGroup;
 
+  constructor(private fb:FormBuilder, private customValidator: CustomvalidationService) {
+    this.registerForm = this.fb.group({
+        name:[ '',[Validators.required]],
+        username:[ '',[Validators.required]],
+        email:[ '',[Validators.required,Validators.email]],
+        contactNo:[ '',[Validators.required,Validators.maxLength(10),Validators.minLength(10),Validators.pattern]],
+        password:[ '',Validators.required],
+        confirmPassword:[ '',[Validators.required]],
+    }
+    )
+    console.log(this.registerForm.controls);
+    
+   }
   ngOnInit(): void {
   }
 
